8986dddbd03b99481e46ba3cb101721fe56bdbf3
Roman Posokhov <posokhovrom@gmail.com>
2022-07-06 21:12:07 +0300

    added touch events

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ const inputs = Array.from(document.querySelectorAll('input'))
 const wrap = document.querySelector('.wrap')
 const numpad = document.querySelector('.numpad')
 
-const inputInfo = { id: 'cm', index: 0, value: '' }
+const onFocus = { id: 'cm', index: 0, value: '' }
 const logic = { cm: { 1: 0.393701, 2: 0.0328084 }, inch: { 0: 2.54, 2: 0.0833333 }, foot: { 1: 12, 0: 30.48 } }
 const calc = e => { for (const [key, operand] of Object.entries(logic[e.id])) inputs[key].value = +(e.value * operand).toFixed(3) }
 
-const numberClick = e => {
-  const num = e.target.textContent
-  inputs[inputInfo.index].focus()
-  if (num === '.' && inputs[inputInfo.index].value.includes('.') || num === '.' && inputs[inputInfo.index].value.length === 0) return
-  num === '<' ? inputs[inputInfo.index].value = inputs[inputInfo.index].value.slice(0, -1) : inputs[inputInfo.index].value += num
-  inputInfo.value = inputs[inputInfo.index].value
-  calc(inputInfo)
+const validate = e => {
+  inputs[onFocus.index].focus()
+  const inp = e.target.textContent
+  const val = inputs[onFocus.index].value
+
+  if (val === "0" && inp !== "." && inp !== "<") {
+    inputs[onFocus.index].value = inp
+  } else if (inp === '<') {
+    inputs[onFocus.index].value = +val.slice(0, -1)
+  } else if (inp === '.' && val.includes('.') || inp === '.' && val.length === 0) {
+    return
+  } else {
+    inputs[onFocus.index].value += inp
+  }
+
+  onFocus.value = inputs[onFocus.index].value
+  calc(onFocus)
 }
 
-const clearAll = () => { for (const input of inputs) input.value = '' }
+const clearAll = () => { for (const input of inputs) input.value = '0' }
 
 document.body.addEventListener('click', () => {
   numpad.classList.add('hidden')
@@ -26,25 +36,23 @@ inputs.forEach((el, i) => el.addEventListener('click', e => {
   e.stopPropagation()
   numpad.classList.remove('hidden')
   wrap.classList.add('moveUp')
-  inputInfo.id = e.target.id;
-  inputInfo.index = i
+  onFocus.id = e.target.id;
+  onFocus.index = i
 }))
+
 
 numpad.addEventListener('click', e => e.stopPropagation())
 
-for (let i = 1; i < 10; i++) numpad.innerHTML += `<p class="default-bg" onclick="numberClick(event)">${i}</p>`
+const numbersArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0', '<']
+for (const val of numbersArr) numpad.innerHTML += `<p class="default-bg" onclick="validate(event)">${val}</p>`
 
-numpad.innerHTML += `<p class="default-bg" onclick="numberClick(event)">.</p>
-<p class="default-bg" onclick="numberClick(event)">0</p>
-<p class="default-bg" onclick="numberClick(event)"><</p>`
+const numbers = Array.from(document.querySelectorAll('.default-bg'));
 
-const buttons = Array.from(document.querySelectorAll('.default-bg'));
-
-buttons.forEach(el => el.addEventListener('touchstart', e => {
+numbers.forEach(el => el.addEventListener('touchstart', e => {
   e.target.classList.add('touch-bg')
   e.target.classList.remove('default-bg')
 }))
-buttons.forEach(el => el.addEventListener('touchend', e => {
+numbers.forEach(el => el.addEventListener('touchend', e => {
   e.target.classList.remove('touch-bg')
   e.target.classList.add('default-bg')
 }))

6e8595e644e1ccf8b3eadc6b132b8d413f16c45a
Roman Posokhov <posokhovrom@gmail.com>
2022-07-06 03:49:49 +0300

    init

--- index.js
+++ index.js
@@ -1,49 +1,37 @@
-const inputFields = Array.from(document.querySelectorAll('input'))
+const inputs = Array.from(document.querySelectorAll('input'))
 const wrap = document.querySelector('.wrap')
 const numpad = document.querySelector('.numpad')
-const inputEventInfo = { id: 'cm', index: 0, value: '' }
+
+const inputInfo = { id: 'cm', index: 0, value: '' }
 const logic = { cm: { 1: 0.393701, 2: 0.0328084 }, inch: { 0: 2.54, 2: 0.0833333 }, foot: { 1: 12, 0: 30.48 } }
-const calc = e => { for (const [key, operand] of Object.entries(logic[e.id])) inputFields[key].value = +(e.value * operand).toFixed(3) }
+const calc = e => { for (const [key, operand] of Object.entries(logic[e.id])) inputs[key].value = +(e.value * operand).toFixed(3) }
+
 const numberClick = e => {
-  inputFields[inputEventInfo.index].value += e.target.textContent
-  inputEventInfo.value = inputFields[inputEventInfo.index].value
-  calc(inputEventInfo)
+  const num = e.target.textContent
+  inputs[inputInfo.index].focus()
+  if (num === '.' && inputs[inputInfo.index].value.includes('.') || num === '.' && inputs[inputInfo.index].value.length === 0) return
+  num === '<' ? inputs[inputInfo.index].value = inputs[inputInfo.index].value.slice(0, -1) : inputs[inputInfo.index].value += num
+  inputInfo.value = inputs[inputInfo.index].value
+  calc(inputInfo)
 }
-const backSpace = () => {
-  inputFields[inputEventInfo.index].value = inputFields[inputEventInfo.index].value.slice(0, -1)
-  inputEventInfo.value = inputFields[inputEventInfo.index].value
-  calc(inputEventInfo)
-}
-const clearAll = () => {
-  for (const input of inputFields) input.value = ''
-}
+
+const clearAll = () => { for (const input of inputs) input.value = '' }
 
 document.body.addEventListener('click', () => {
   numpad.classList.add('hidden')
   wrap.classList.remove('moveUp')
 })
 
-// inputFields.forEach((el, i) => el.addEventListener('focus', e => {
-//   e.stopPropagation()
-//   numpad.classList.remove('hidden')
-//   wrap.classList.add('moveUp')
-//   inputEventInfo.id = e.target.id;
-//   inputEventInfo.index = i
-// }))
-
-inputFields.forEach((el, i) => el.addEventListener('click', e => {
+inputs.forEach((el, i) => el.addEventListener('click', e => {
   e.stopPropagation()
   numpad.classList.remove('hidden')
   wrap.classList.add('moveUp')
-  inputEventInfo.id = e.target.id;
-  inputEventInfo.index = i
+  inputInfo.id = e.target.id;
+  inputInfo.index = i
 }))
 
+numpad.addEventListener('click', e => e.stopPropagation())
 
-numpad.addEventListener('click', e => {
-  e.stopPropagation()
-})
-for (let i = 1; i < 10; i++)
-  numpad.innerHTML += `<p onclick="numberClick(event)">${i}</p>`
+for (let i = 1; i < 10; i++) numpad.innerHTML += `<button onclick="numberClick(event)">${i}</button>`
 
-numpad.innerHTML += `<p onclick="backSpace()"><</p><p onclick="numberClick(event)">0</p><p onclick="numberClick(event)">.</p>`
+numpad.innerHTML += `<button onclick="numberClick(event)"><</button><button onclick="numberClick(event)">0</button><button onclick="numberClick(event)">.</button>`
